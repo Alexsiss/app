d756a29cf962235199ac78632748f3e4bd59d547
Alexsis jimenez <antoniojimenezjimenez197737.gmail>
2016-06-27 10:45:42 -0600

    new file READEME

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 <h1>Pagina principal</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quod eius, sequi ducimus. Voluptate ab esse maiores corporis. Soluta laborum dolorum ratione, reiciendis molestias, impedit repellat debitis odio autem maxime?</p>
 <h2>Encabezado tipo H2</h2>
+<p>
+	
+</p>
 	
 </body>
 </html>
