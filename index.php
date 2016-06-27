9359bc4efa9278febf23ca0d5fd207f7b057703f
Alexsis jimenez <antoniojimenezjimenez197737.gmail>
2016-06-27 10:24:23 -0600

    Repositorio inicial

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>aplicaciones</title>
+</head>
+<body>
+<h1>Pagina principal</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quod eius, sequi ducimus. Voluptate ab esse maiores corporis. Soluta laborum dolorum ratione, reiciendis molestias, impedit repellat debitis odio autem maxime?</p>
+<h2>Encabezado tipo H2</h2>
+	
+</body>
+</html>
